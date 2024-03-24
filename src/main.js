059e6df52573871656adee9720fc0ea758cbf027
SergioAle210 <ore221122@uvg.edu.gt>
2024-03-24 17:15:29 +0000

    Feature: Swagger implemented

--- src/main.js
+++ src/main.js
@@ -4,12 +4,15 @@ import {
 // eslint-disable-next-line import/extensions
 } from '../db.js'
 import cors from 'cors'
+import swaggerUi from 'swagger-ui-express'
+import swaggerDocument from './swagger.json' // Ruta al archivo de definición de Swagger
 
 const app = express()
 const port = 22596
 
 app.use(express.json())
 app.use(cors())
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
